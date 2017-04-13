d14ef874eb00572becc8ca162e13f7117ac69172
luis-villase <luis.villase@gmail.com>
2017-04-13 04:06:29 -0500

    ?????

--- Humanitak/webapp/Controllers/CalcController.cs
+++ Humanitak/webapp/Controllers/CalcController.cs
@@ -25,7 +25,7 @@ namespace SmartAdminMvc.Controllers
 
         [HttpPost]
         public PartialViewResult _CalcData(SalaryComponentsViewModel model) {
-            return PartialView("_CalcData", DeductionHelper.GetCalculatedSalaryValues(model.WantedSalary, model.TotalDays, model.HasImss == "on"));
+            return PartialView("~/Views/Calc/_CalcData.cshtml", DeductionHelper.GetCalculatedSalaryValues(model.WantedSalary, model.TotalDays, model.HasImss == "on"));
         }
 
         [HttpPost]
